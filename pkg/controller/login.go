8911791d53362ff3170194b49a3efc65f56171ce
Shane Bryzak <sbryzak@gmail.com>
2022-12-14 15:09:29 +1000

    Added login controller and wired it up

---
+++ pkg/controller/login.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"context"
+	"github.com/codeready-toolchain/sandbox-auth/gen/login"
+	"github.com/codeready-toolchain/sandbox-auth/pkg/application"
+)
+
+type loginController struct {
+	app application.Application
+}
+
+// NewLogin returns the login service implementation.
+func NewLoginController(app application.Application) login.Service {
+	return &loginController{
+		app: app,
+	}
+}
+
+func (l loginController) Login(ctx context.Context, criteria *login.LoginCriteria) (res *login.LoginResult, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l loginController) Callback(ctx context.Context, criteria *login.LoginCallbackCriteria) (res *login.LoginResult, err error) {
+	//TODO implement me
+	panic("implement me")
+}
